e2db1fcd8704f6a8806d2b55a0a82e03e3d32777
levi.angyal <pedukk13>
2021-11-07 17:45:54 +0100

    3. Longest Substring Without Repeating Characters 992. Subarrays with K Different Integers

--- src/main/java/leetcode/problems/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/leetcode/problems/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,33 +1,46 @@
 package leetcode.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters
+ *
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *
+ * Constraints:
+ *
+ * 0 <= s.length <= 5 * 10^4
+ * s consists of English letters, digits, symbols and spaces.
+ *
+ * Tags: Sliding Window
+ */
 public class LongestSubstringWithoutRepeatingCharacters {
 
     public int lengthOfLongestSubstring(String s) {
-        if (s.isEmpty()) return 0;
-        if (s.length() == 1) return 1;
+        if (s.length() < 2) {
+            return s.length();
+        }
         int longest = 1;
-        int current = 1;
         int x = 0; // inclusive
         int y = 1; // exclusive
+        Map<Character, Integer> map = new HashMap<Character, Integer>() {{ put(s.charAt(0), 0); }};
         while (y < s.length()) {
             char ch = s.charAt(y);
-            String sub = s.substring(x, y);
-            if (sub.indexOf(ch) == -1) { // if not found
-                y++;
-                current++;
-                if (current > longest) {
-                    longest = current;
-                }
-            } else {
-                x++;
-                current--;
+            Integer found = map.get(ch);
+            if (found != null && found >= x) {
+                x = (found + 1);
             }
+            map.put(ch, y);
+            longest = Math.max(longest, y - x + 1);
+            y++;
         }
         return longest;
     }
 
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters l = new LongestSubstringWithoutRepeatingCharacters();
+        System.out.println(l.lengthOfLongestSubstring("bbtablud")); // expected: 6
         System.out.println(l.lengthOfLongestSubstring("abcabcbb")); // expected: 3
         System.out.println(l.lengthOfLongestSubstring("bbbbb")); // expected: 1
         System.out.println(l.lengthOfLongestSubstring("pwwkew")); // expected: 3
---
+++ src/main/java/leetcode/problems/Subarrays_with_K_Different_Integers.java
@@ -0,0 +1,85 @@
+package leetcode.problems;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/subarrays-with-k-different-integers/
+ *
+ * Given an integer array nums and an integer k, return the number of good subarrays of nums.
+ *
+ * A good array is an array where the number of different integers in that array is exactly k.
+ *
+ * For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+ * A subarray is a contiguous part of an array.
+ *
+ * Constraints:
+ *      1 <= nums.length <= 2 * 10^4
+ *      1 <= nums[i], k <= nums.length
+ */
+public class Subarrays_with_K_Different_Integers {
+
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k-1);
+    }
+
+    public int helper(int[] nums, int k) {
+        int count = 0;
+        int x = 0;
+        int y = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // number -> counts in current streak
+        int maxIdx = -1;
+        while (y < nums.length) {
+            if (y > maxIdx) {
+                map.put(nums[y], map.getOrDefault(nums[y], 0) + 1);
+                maxIdx = y;
+            }
+            if (map.size() <= k) {
+                int len = y-x+1;
+                count += len;
+                y++;
+            } else /* if (map.size() > k) */ {
+                if (1 < map.get(nums[x])) {
+                    map.put(nums[x], map.get(nums[x])-1 );
+                } else {
+                    map.remove(nums[x]);
+                }
+                x++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Subarrays_with_K_Different_Integers s = new Subarrays_with_K_Different_Integers();
+
+        System.out.println(s.subarraysWithKDistinct(new int[] {1,2,1,2,3}, 2)); // Output: 7
+        System.out.println(s.subarraysWithKDistinct(new int[] {1,2,1,3,4}, 3)); // Output: 3
+        System.out.println(s.subarraysWithKDistinct(new int[] {2,1,1,1,2}, 1)); // Output: 8
+        System.out.println(s.subarraysWithKDistinct(new int[] {2,2,1,2,2,2,1,1}, 2)); // Output: 23
+    }
+
+    /**
+     * Naive approach, also very slow
+     */
+    public int subarraysWithKDistinct0(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            Set<Integer> set = new HashSet<>(Collections.singletonList(nums[i]));
+            if (k == 1) {
+                count++;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                set.add(nums[j]);
+                if (set.size() == k) {
+                    count++;
+                } else if (set.size() > k) {
+                    // overflow
+                    break;
+                }
+            }
+
+        }
+        return count;
+    }
+
+}
